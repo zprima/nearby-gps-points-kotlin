c67a95ef0248834260341a7ae0ffb60679c356ac
Primož Žnidar <sg.prima@gmail.com>
2020-10-17 23:03:24 +0200

    Added code comments

--- main.kt
+++ main.kt
@@ -5,6 +5,8 @@ import okio.Okio
 import java.nio.file.Paths
 import kotlin.math.*
 
+// Class that will contain our mountain data from json file
+// @Json notation is not needed if the variable name is same as json name
 class Mountain(
         @Json(name = "name") val name: String,
         @Json(name = "country") val country: String,
@@ -15,6 +17,7 @@ class Mountain(
 )
 
 // Example of using custom adapter in Moshi
+// in case we want to control or modify the values while serializing
 //
 // val moshi = Moshi.Builder()
 //        .add(MountainAdapter())
@@ -53,12 +56,14 @@ class Mountain(
 //    }
 //}
 
+// Helper class to store latitude and longitude pair
 class GpsPoint(
         val lon: Double,
         val lat: Double
 )
 
-const val earthRadiusKm: Double = 6371.0;  // earth's mean radius, km
+// earth's mean radius in km
+const val earthRadiusKm: Double = 6371.0
 
 // Haversine - "as the crow flies" algorithm to determine distance between 2 gps points
 // https://www.movable-type.co.uk/scripts/latlong.html
@@ -69,32 +74,33 @@ fun haversine(currentPoint: GpsPoint, targetPoint: GpsPoint): Double {
     val dLat = Math.toRadians(targetPoint.lat - currentPoint.lat)
     val dLon = Math.toRadians(targetPoint.lon - currentPoint.lon)
 
-    val a = sin(dLat/2) * sin(dLat/2) +
-            cos(lat1) * cos(lat2) * sin(dLon/2) * sin(dLon/2)
-    val c = 2 * kotlin.math.atan2(sqrt(a), sqrt(1-a))
+    val a = sin(dLat / 2) * sin(dLat / 2) +
+            cos(lat1) * cos(lat2) * sin(dLon / 2) * sin(dLon / 2)
+    val c = 2 * atan2(sqrt(a), sqrt(1 - a))
 
-    return round(earthRadiusKm * c * 1000 ) /  1000.0
+    return round(earthRadiusKm * c * 1000) / 1000.0
 }
 
+// Helper class that contains both mountain record and the distance to it
 class MountainWithDistance(
         val mountain: Mountain,
         val distance: Double
 )
 
-fun main(){
-    // read json file
+fun main() {
+    // read json file, with help of Moshi serializer
     val moshi = Moshi.Builder()
             .addLast(KotlinJsonAdapterFactory())
             .build()
 
-    // option 1
+    // option 1 - direct
     val type = newParameterizedType(List::class.java, Mountain::class.java)
     val jsonAdapter: JsonAdapter<List<Mountain>> = moshi.adapter(type)
     val reader = JsonReader.of(Okio.buffer(Okio.source(Paths.get("src", "main", "kotlin", "data", "slovenia_peaks.json").toAbsolutePath().toFile())))
     val mountains: List<Mountain> = jsonAdapter.fromJson(reader)!!
     reader.close()
 
-    // options 2
+    // options 2 - with reader going step by step. no difference compared to option 1 above
     //    val jsonAdapter: JsonAdapter<Mountain> = moshi.adapter<Mountain>(Mountain::class.java)
     //    val reader = JsonReader.of(Okio.buffer(Okio.source(Paths.get("src","main", "kotlin", "data", "slovenia_peaks.json").toAbsolutePath().toFile())))
     //    reader.beginArray()
@@ -105,19 +111,13 @@ fun main(){
     //    reader.endArray()
     //    reader.close()
 
-    //    Output
-    //    for(mountain in mountains){
-    //        println(mountain.name)
-    //    }
-
-
     // have 2 random geo points to simulate current position
     val currLat: Double = 46.2194828
     val currLon: Double = 15.2719759
     val currentPoint: GpsPoint = GpsPoint(lat = currLat, lon = currLon)
 
     // find entries within 10km radius
-    val targetRadius: Double = 10.0 // radius of bounding circle in kilometers
+    val targetRadius: Double = 10.0
 
     val rad2radiusRatio = targetRadius / earthRadiusKm
 
@@ -126,14 +126,10 @@ fun main(){
     val maxLon = currLon + Math.toDegrees(asin(rad2radiusRatio) / cos(Math.toRadians(currLat)))
     val minLon = currLon - Math.toDegrees(asin(rad2radiusRatio) / cos(Math.toRadians(currLat)))
 
+    // find only those mountains that are withing the bounding box
     val mountainsWithinRange: List<Mountain> = mountains.filter { mountain ->
         mountain.latitude in minLat..maxLat && mountain.longitude in minLon..maxLon
     }
-
-    //    Output
-    //    for(mountain in mountainsWithinRange){
-    //        println(mountain.name)
-    //    }
 
     // sort by distance with haversine formula - "as the crow flies"
     val sortedMountainWithDistance = mountainsWithinRange.map { mountain ->
@@ -142,7 +138,7 @@ fun main(){
                 targetPoint = GpsPoint(lon = mountain.longitude, lat = mountain.latitude)
         )
 
-        MountainWithDistance(mountain,distance)
+        MountainWithDistance(mountain, distance)
     }.sortedBy { mountainWithDistance -> mountainWithDistance.distance }
 
     // output results
